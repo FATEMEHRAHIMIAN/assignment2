9953b00831e50a1d1e7a35f4da1ebba74aa14ff0
FATEMEHRAHIMIAN <110339993+FATEMEHRAHIMIAN@users.noreply.github.com>
2022-08-22 14:53:15 +0430

    Add files via upload

--- 1_login.py
+++ 1_login.py
@@ -6,10 +6,10 @@ password = '1234'
 counter =3
 while counter <4:
     user = input("Enter your username")
-    password_us=int(input("Enter your pass"))
+    password_us=input("Enter your pass")
     if user == username and password_us == password :
         print("successful..")
-        continue
+        break
     elif counter ==  1:
         print("blocked..")   
     else:
